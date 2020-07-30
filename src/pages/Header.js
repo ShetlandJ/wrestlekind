d485fa6ec16df44b7dc6f33f248dbaddf44b6491
James <james@jastewart.co.uk>
2020-07-30 18:29:14 +0100

    initial commit

---
+++ src/pages/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    Nav, Navbar, Row, Col, Container
+} from 'react-bootstrap';
+
+function Header() {
+    return (
+        <Navbar bg="light" expand="lg" className="d-flex justify-content-between">
+            <div>
+                <Navbar.Brand href="#home" className="mr-0">
+                    <img
+                        src={process.env.PUBLIC_URL + "/WK_logo.png"}
+                        alt="logo"
+                        className="wk-logo"
+                    />
+                </Navbar.Brand>
+            </div>
+
+            <div>
+                <Nav className="m-auto">
+                    <p className="header-text">WrestleKind</p>
+                </Nav>
+            </div>
+
+            <div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            </div>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="m-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    Nav, Navbar, Row, Col, Container
+} from 'react-bootstrap';
+
+function Home() {
+    return (
+        <h3>HOME!</h3>
+    )
+}
+
+export default Home;
